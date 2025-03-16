64fe2727b66d4ae02eee268126abaf443dd177d1
Sehnder <AndrewDLance@gmail.com>
2025-03-16 15:05:26 -0600

    I am a book bot master

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from stats import get_num_words, get_chars_dict, add_count_as_value
+import sys
+
+
+
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    ####Check for errors first
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+    
+    book_path = str(sys.argv[1])
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
